6257b5541eadacd26a40c10748678587a7185c66
mateuszzalewski <MateuszZalewsky@gmail.com>
2021-10-17 14:34:41 +0200

    add nested accessinfo field to book serializer

--- app/books/models/access_info.py
+++ app/books/models/access_info.py
@@ -3,45 +3,45 @@ from .book import Book
 
 
 class AccessInfo(models.Model):
-    book = models.OneToOneField(Book, on_delete=models.CASCADE)
-    country = models.CharField(max_length=100)
-    viewability = models.CharField(max_length=100)
-    accessViewStatus = models.CharField(max_length=100)
+    book = models.OneToOneField(Book, on_delete=models.CASCADE, related_name='accessInfo')
+    country = models.CharField(max_length=100, null=True)
+    viewability = models.CharField(max_length=100, null=True)
+    accessViewStatus = models.CharField(max_length=100, null=True)
     embeddable = models.BooleanField()
     publicDomain = models.BooleanField()
-    webReaderLink = models.URLField()
-    textToSpeechPermission = models.CharField(max_length=100)
+    webReaderLink = models.URLField(null=True)
+    textToSpeechPermission = models.CharField(max_length=100, null=True)
     quoteSharingAllowed = models.BooleanField()
 
 
 class Epub(models.Model):
-    accessInfo = models.OneToOneField(AccessInfo, on_delete=models.CASCADE)
-    downloadLink = models.URLField()
-    acsTokenLink = models.URLField()
+    accessInfo = models.OneToOneField(AccessInfo, on_delete=models.CASCADE, related_name='epub')
+    downloadLink = models.URLField(null=True)
+    acsTokenLink = models.URLField(null=True)
     isAvailable = models.BooleanField()
 
 
 class Pdf(models.Model):
-    accessInfo = models.OneToOneField(AccessInfo, on_delete=models.CASCADE)
-    downloadLink = models.URLField()
-    acsTokenLink = models.URLField()
+    accessInfo = models.OneToOneField(AccessInfo, on_delete=models.CASCADE, related_name='pdf')
+    downloadLink = models.URLField(null=True)
+    acsTokenLink = models.URLField(null=True)
     isAvailable = models.BooleanField()
 
 
 class DownloadAccess(models.Model):
-    accessInfo = models.OneToOneField(AccessInfo, on_delete=models.CASCADE)
+    accessInfo = models.OneToOneField(AccessInfo, on_delete=models.CASCADE, related_name='downloadAccess')
     kind = models.CharField(max_length=100)
-    volumeId = models.CharField(max_length=100)
+    volumeId = models.CharField(max_length=100, null=True)
     restricted = models.BooleanField()
     deviceAllowed = models.BooleanField()
     justAcquired = models.BooleanField()
     maxDownloadDevices = models.IntegerField()
     downloadsAcquired = models.IntegerField()
-    nonce = models.CharField(max_length=100)
-    source = models.CharField(max_length=100)
-    reasonCode = models.CharField(max_length=100)
+    nonce = models.CharField(max_length=100, null=True)
+    source = models.CharField(max_length=100, null=True)
+    reasonCode = models.CharField(max_length=100, null=True)
     message = models.TextField()
-    signature = models.CharField(max_length=100)
+    signature = models.CharField(max_length=100, null=True)
 
 
 
--- app/books/serializers.py
+++ app/books/serializers.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
-
 from rest_framework import serializers
 from .models import Book, SearchInfo
+from .models.access_info import AccessInfo, Epub, Pdf, DownloadAccess
 
 
 class NonNullModelSerializer(serializers.ModelSerializer):
@@ -9,6 +9,34 @@ class NonNullModelSerializer(serializers.ModelSerializer):
         result = super().to_representation(instance)
         # filter out null values with list comprehension
         return OrderedDict([(key, result[key]) for key in result if result[key] is not None])
+
+
+class DownloadAccessSerializer(NonNullModelSerializer):
+    class Meta:
+        model = DownloadAccess
+        exclude = ('accessInfo', )
+
+
+class PdfSerializer(NonNullModelSerializer):
+    class Meta:
+        model = Pdf
+        exclude = ('accessInfo', )
+
+
+class EpubSerializer(NonNullModelSerializer):
+    class Meta:
+        model = Epub
+        exclude = ('accessInfo', )
+
+
+class AccessInfoSerializer(NonNullModelSerializer):
+    downloadAccess = DownloadAccessSerializer(required=False)
+    pdf = PdfSerializer(required=False)
+    epub = EpubSerializer(required=False)
+
+    class Meta:
+        model = AccessInfo
+        exclude = ('book', )
 
 
 class SearchInfoSerializer(NonNullModelSerializer):
@@ -19,6 +47,7 @@ class SearchInfoSerializer(NonNullModelSerializer):
 
 class BookSerializer(NonNullModelSerializer):
     searchInfo = SearchInfoSerializer(required=False)
+    accessInfo = AccessInfoSerializer(required=False)
 
     class Meta:
         model = Book
@@ -30,8 +59,27 @@ class BookSerializer(NonNullModelSerializer):
         else:
             search_info_data = None
 
+        access_info_data = pdf_data = epub_data = download_access_data = None
+
+        if 'accessInfo' in validated_data:
+            access_info_data = validated_data.pop('accessInfo')
+            if 'pdf' in access_info_data:
+                pdf_data = access_info_data.pop('pdf')
+            if 'epub' in access_info_data:
+                epub_data = access_info_data.pop('epub')
+            if 'downloadAccess' in access_info_data:
+                download_access_data = access_info_data.pop('downloadAccess')
+
         book = Book.objects.create(**validated_data)
 
         if search_info_data:
             SearchInfo.objects.create(book=book, **search_info_data)
+        if access_info_data:
+            access_info = AccessInfo.objects.create(book=book, **access_info_data)
+            if pdf_data:
+                Pdf.objects.create(accessInfo=access_info, **pdf_data)
+            if epub_data:
+                Epub.objects.create(accessInfo=access_info, **epub_data)
+            if download_access_data:
+                DownloadAccess.objects.create(accessInfo=access_info, **download_access_data)
         return book
