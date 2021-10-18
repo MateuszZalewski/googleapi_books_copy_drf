b31ad17d0d29196bcc3d6b4fee7165bf199b4736
mateuszzalewski <MateuszZalewsky@gmail.com>
2021-10-18 16:24:34 +0200

    add ability to POST multiple books at once

--- app/books/views.py
+++ app/books/views.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, status
 import django_filters.rest_framework
+from rest_framework.response import Response
 
 from .models import Book
 from .serializers import BookSerializer
@@ -18,6 +19,20 @@ class BookViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.OrderingFilter, django_filters.rest_framework.DjangoFilterBackend)
     ordering_fields = ('volumeInfo__publishedDate',)
     filterset_class = BookFilter
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.get('items', request.data)
+        many = isinstance(data, list)
+        print (data, many)
+        serializer = self.get_serializer(data=data, many=many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED,
+                headers=headers
+        )
 
     def get_queryset(self):
         return Book.objects.all()
