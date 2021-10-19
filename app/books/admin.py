daa79ff7aeec2c0bd570b30519907c26315c80ee
mateuszzalewski <MateuszZalewsky@gmail.com>
2021-10-19 18:15:53 +0200

    remove old models from admin

--- app/books/admin.py
+++ app/books/admin.py
@@ -1,18 +1,8 @@
 from django.contrib import admin
-from .models import *
+from .models import Book, Author, Category
 
 # Register your models here.
 
-admin.site.register(AccessInfo)
-admin.site.register(Epub)
-admin.site.register(Pdf)
-admin.site.register(DownloadAccess)
-admin.site.register(SearchInfo)
+admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Category)
-admin.site.register(VolumeInfo)
-admin.site.register(IndustryIdentifier)
-admin.site.register(Dimensions)
-admin.site.register(ReadingModes)
-admin.site.register(PanelizationSummary)
-admin.site.register(ImageLinks)
